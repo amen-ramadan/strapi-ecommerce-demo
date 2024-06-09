11856b5473dd4caf9e0174d50a6c8edd6c4873bf
amen ramadan <amen@MacbookShops-MacBook-Pro.local>
2024-06-09 06:26:14 +0300

    first commit

---
+++ src/api/product-amen-test/routes/product-amen-test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-amen-test router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::product-amen-test.product-amen-test');
---
+++ src/api/product-amen-test/services/product-amen-test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-amen-test service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::product-amen-test.product-amen-test');
